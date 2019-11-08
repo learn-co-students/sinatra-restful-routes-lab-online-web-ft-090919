5ae4dd1cb0d09527b59135b10fab861dc73253a6
Alyssa Yates <bosslys@icloud.com>
2019-11-08 17:26:52 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,36 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes' do
+  @recipes = Recipe.all
+
+  erb :index
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  post "/recipes" do
+  recipe = Recipe.new(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+
+  if params[:user] == "" || params[:ingredients] == "" || params[:cook_time] == ""
+    redirect "/failure"
+  else
+    recipe.save
+    redirect "/index"
+    end
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+
+    erb :edit
+  end
+
+  get '/recipes/new' do
+    erb :recipe
+  end
 
 end
